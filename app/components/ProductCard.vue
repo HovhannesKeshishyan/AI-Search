b9f2a2933726b9056ce58f5de98fc009a30af33b
HovhannesK <hovhanneskeshishyan@gmail.com>
2025-11-19 21:19:17 +0400

    fix produc card styles

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+defineProps<{
+  product: Product;
+}>();
+</script>
+
+<template>
+<div class="product-card">
+  <div class="image-wrapper">
+    <NuxtImg :src="product.image" width="300" height="200" quality="80" fit="cover" :alt="product.title" />
+  </div>
+
+  <div class="product-card-body">
+    <h2 class="product-title">{{ product.title }}</h2>
+
+    <h3 class="product-description">{{ product.description }}</h3>
+
+    <div class="product-price">${{ Number(product.price).toFixed(2) }}</div>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.product-card {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 0 8px 1px darkcyan;
+  border-radius: pxToRem(20px);
+  overflow: hidden;
+}
+
+.image-wrapper {
+  border-radius: pxToRem(10px) pxToRem(10px) 0 0;
+  height: pxToRem(200px);
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.product-card-body {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: pxToRem(5px);
+  padding: pxToRem(10px) pxToRem(15px);
+}
+
+.product-title {
+  color: white;
+  font-size: pxToRem(24px);
+  line-height: pxToRem(28px);
+}
+
+.product-description {
+  font-size: pxToRem(20px);
+  color: #9d9d9d;
+}
+
+.product-price {
+  margin-top: auto;
+  font-size: pxToRem(24px);
+  color: darkcyan;
+}
+</style>
--- app/components/ProductsList.vue
+++ app/components/ProductsList.vue
@@ -4,27 +4,13 @@ defineProps<{ products: Product[] }>();
 
 <template>
 <div class="products-list">
-  <NuxtLink v-for="product in products" :key="product.id" :to="`/product/${product.id}`" class="product-card"
+  <NuxtLink 
+     v-for="product in products"
+     :key="product.id"
+     :to="`/product/${product.id}`"
+     class="product-item"
     :data-has-embeddings="!!product.embeddings?.length">
-    <Card>
-      <template #header>
-        <div class="image-wrapper">
-          <NuxtImg :src="product.image" width="300" height="200" quality="80" fit="cover" :alt="product.title" />
-        </div>
-      </template>
-
-      <template #title>
-        <h2 class="product-title">{{ product.title }}</h2>
-      </template>
-
-      <template #content>
-        <h3 class="product-description">{{ product.description }}</h3>
-      </template>
-
-      <template #footer>
-        <div class="product-price">${{ Number(product.price).toFixed(2) }}</div>
-      </template>
-    </Card>
+    <ProductCard :product="product"/>
   </NuxtLink>
 </div>
 </template>
@@ -35,39 +21,9 @@ defineProps<{ products: Product[] }>();
   grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   gap: pxToRem(16px);
   padding: pxToRem(20px) 0;
-}
 
-.products-list a {
-  text-decoration: none;
-}
-
-.product-card {
-  width: 100%;
-}
-
-.image-wrapper {
-  border-radius: pxToRem(10px) pxToRem(10px) 0 0;
-  overflow: hidden;
-  height: pxToRem(200px);
-  position: relative;
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  a {
+    text-decoration: none;
   }
-}
-
-.product-title {
-  font-size: pxToRem(34px);
-}
-
-.product-description {
-  font-size: pxToRem(20px);
-}
-
-.product-price {
-  font-size: pxToRem(24px);
-  color: darkcyan;
 }
 </style>
