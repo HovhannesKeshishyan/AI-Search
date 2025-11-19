69f51fe7183e98d985d8807404e2597ab749c2a8
HovhannesK <hovhanneskeshishyan@gmail.com>
2025-11-20 00:13:02 +0400

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,30 +2,42 @@ import Aura from "@primeuix/themes/aura";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+
   devtools: { enabled: true },
+
   modules: [
     "@nuxt/eslint",
     "@nuxt/icon",
     "@nuxt/image",
     "@primevue/nuxt-module",
   ],
+
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+    },
+  },
+
   css: ["~/assets/css/global.scss"],
+
   image: {
     domains: ["https://images.unsplash.com"],
   },
+
   runtimeConfig: {
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
     redisUrl: process.env.REDIS_URL,
     redisDbKey: process.env.REDIS_DATA_KEY,
     public: {},
   },
+
   primevue: {
     options: {
       theme: {
         preset: Aura,
         options: {
-            darkModeSelector: false
-        }
+          darkModeSelector: false,
+        },
       },
     },
   },
