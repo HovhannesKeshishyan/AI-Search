44cae1ba97ea7a77fe3a78746e13135f9c11fd3c
HovhannesK <hovhanneskeshishyan@gmail.com>
2025-11-20 19:30:41 +0400

    fix products-list styles in mobile view

--- app/components/ProductsList.vue
+++ app/components/ProductsList.vue
@@ -16,11 +16,17 @@ defineProps<{ products: Product[] }>();
 </template>
 
 <style lang="scss" scoped>
+@use "~/assets/css/breackpoints.scss";
+
 .products-list {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   gap: pxToRem(30px) pxToRem(16px);
   padding: pxToRem(20px) 0;
+
+  @media screen and (max-width: breackpoints.$mobile) {
+    grid-template-columns: 1fr;
+  }
 
   a {
     text-decoration: none;
